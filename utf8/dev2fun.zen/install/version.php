ae38eb05d740c9e884a16946e0f7815173160691
darkfriend <maslov.tlt@gmail.com>
2020-10-26 02:48:45 +0300

    Исправлен вывод ленты для utf8

--- utf8/dev2fun.zen/install/version.php
+++ utf8/dev2fun.zen/install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "1.0.6",
-    "VERSION_DATE" => "2020-01-04 10:00:00",
+    "VERSION" => "1.0.7",
+    "VERSION_DATE" => "2020-10-26 10:00:00",
 );
--- win1251/dev2fun.zen/install/components/dev2fun/yandex.zen/templates/.default/template.php
+++ win1251/dev2fun.zen/install/components/dev2fun/yandex.zen/templates/.default/template.php
@@ -1,8 +1,11 @@
 <?php
 /** @var Dev2funYandexZenComponent $this */
-//header('Content-Type: application/rss+xml; charset=utf-8');
 header('Content-Type: text/xml; charset=utf-8');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
+$sourceCharset = 'cp1251';
+if(\BX_UTF===true) {
+    $sourceCharset = 'utf8';
+}
 ?>
     <rss version="2.0"
          xmlns:content="http://purl.org/rss/1.0/modules/content/"
@@ -12,56 +15,56 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
          xmlns:georss="http://www.georss.org/georss">
         <channel>
             <?php if (!empty($arResult['SITE']["SITE_NAME"])) { ?>
-                <title><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_NAME"], 'cp1251', 'utf8') ?></title>
+                <title><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_NAME"], $sourceCharset, 'utf8') ?></title>
             <?php } ?>
             <?php if (!empty($arResult['SITE']["SITE_URL"])) { ?>
-                <link><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_URL"], 'cp1251', 'utf8') ?></link>
+                <link><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_URL"], $sourceCharset, 'utf8') ?></link>
             <?php } ?>
             <?php if (!empty($arResult['SITE']["SITE_DESCRIPTION"])) { ?>
-                <description><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_DESCRIPTION"], 'cp1251', 'utf8'); ?></description>
+                <description><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_DESCRIPTION"], $sourceCharset, 'utf8'); ?></description>
             <?php } ?>
             <?php if (!empty($arResult['SITE']["SITE_LANG"])) { ?>
-                <language><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_LANG"], 'cp1251', 'utf8'); ?></language>
+                <language><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_LANG"], $sourceCharset, 'utf8'); ?></language>
             <?php } ?>
             <?php foreach ($arResult["ITEMS"] as $arItem): ?>
                 <item>
                     <?php if (!empty($arItem["NAME"])) { ?>
-                        <title><?= $APPLICATION->ConvertCharset($arItem["NAME"], 'cp1251', 'utf8') ?></title>
+                        <title><?= $APPLICATION->ConvertCharset($arItem["NAME"], $sourceCharset, 'utf8') ?></title>
                     <?php } ?>
                     <?php if (!empty($arItem["DETAIL_PAGE_URL"])) { ?>
-                        <link><?= $APPLICATION->ConvertCharset($arItem["DETAIL_PAGE_URL"], 'cp1251', 'utf8') ?></link>
+                        <link><?= $APPLICATION->ConvertCharset($arItem["DETAIL_PAGE_URL"], $sourceCharset, 'utf8') ?></link>
                     <?php } ?>
                     <?php if (!empty($arItem["DATE_CREATE"])) { ?>
-                        <pubDate><?= $APPLICATION->ConvertCharset($arItem["DATE_CREATE"], 'cp1251', 'utf8') ?></pubDate>
+                        <pubDate><?= $APPLICATION->ConvertCharset($arItem["DATE_CREATE"], $sourceCharset, 'utf8') ?></pubDate>
                     <?php } ?>
                     <?php if (!empty($arResult["RATING"])) { ?>
-                        <media:rating scheme="urn:simple"><?= $APPLICATION->ConvertCharset($arResult['RATING'], 'cp1251', 'utf8') ?></media:rating>
+                        <media:rating scheme="urn:simple"><?= $APPLICATION->ConvertCharset($arResult['RATING'], $sourceCharset, 'utf8') ?></media:rating>
                     <?php } ?>
                     <?php if (!empty($arResult['SITE']["SITE_NAME"])) { ?>
-                        <author><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_NAME"], 'cp1251', 'utf8'); ?></author>
+                        <author><?= $APPLICATION->ConvertCharset($arResult['SITE']["SITE_NAME"], $sourceCharset, 'utf8'); ?></author>
                     <?php } ?>
                     <?php if (!empty($arResult["CATEGORY"])) { ?>
                         <?php foreach ($arResult["CATEGORY"] as $category): ?>
-                            <category><?= $APPLICATION->ConvertCharset($category, 'cp1251', 'utf8'); ?></category>
+                            <category><?= $APPLICATION->ConvertCharset($category, $sourceCharset, 'utf8'); ?></category>
                         <?php endforeach; ?>
                     <?php } ?>
                     <?php if (!empty($arItem["MEDIA"])) { ?>
                         <?php foreach ($arItem["MEDIA"] as $media) { ?>
                             <enclosure
-                                url="<?= $APPLICATION->ConvertCharset($media['url'], 'cp1251', 'utf8') ?>"
-                                type="<?= $APPLICATION->ConvertCharset($media['type'], 'cp1251', 'utf8') ?>"
+                                url="<?= $APPLICATION->ConvertCharset($media['url'], $sourceCharset, 'utf8') ?>"
+                                type="<?= $APPLICATION->ConvertCharset($media['type'], $sourceCharset, 'utf8') ?>"
                             />
                         <?php } ?>
                     <?php } ?>
                     <?php if (!empty($arItem["PREVIEW_TEXT"])) { ?>
                         <description><![CDATA[
-                            <?= $APPLICATION->ConvertCharset($arItem["PREVIEW_TEXT"], 'cp1251', 'utf8'); ?>
+                            <?= $APPLICATION->ConvertCharset($arItem["PREVIEW_TEXT"], $sourceCharset, 'utf8'); ?>
                             ]]>
                         </description>
                     <?php } ?>
                     <?php if (!empty($arItem["DETAIL_TEXT"])) { ?>
                         <content:encoded><![CDATA[
-                            <?= $APPLICATION->ConvertCharset($arItem["DETAIL_TEXT"], 'cp1251', 'utf8'); ?>
+                            <?= $APPLICATION->ConvertCharset($arItem["DETAIL_TEXT"], $sourceCharset, 'utf8'); ?>
                             ]]>
                         </content:encoded>
                     <?php } ?>
